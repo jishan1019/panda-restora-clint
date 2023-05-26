76d2d7e427c573adae36e72d23763aa5d380cb18
Your Name <imranislamjishan200@gmaile.com>
2023-05-26 20:04:40 +0600

    custom hook add done

--- src/component/HomeLayout/FromOurMenuSection/FromOurMenu.jsx
+++ src/component/HomeLayout/FromOurMenuSection/FromOurMenu.jsx
@@ -1,19 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import SectionTitel from '../../ShareLayout/SectionTitle/SectionTitel';
 import MenuitemCard from '../../ShareLayout/MenuItemCard/MenuitemCard';
+import useMenu from '../../../CustomHooks/useMenu';
 
 const FromOurMenu = () => {
+    // const [menu, setMenu] = useState([])
+    // useEffect(() => {
+    //     fetch('menu.json')
+    //         .then(res => res.json())
+    //         .then(items => {
+    //             const popularData = items.filter(item => item.category === 'popular')
+    //             setMenu(popularData)
+    //         })
+    // }, [])
 
-    const [menu, setMenu] = useState([])
-
-    useEffect(() => {
-        fetch('menu.json')
-            .then(res => res.json())
-            .then(items => {
-                const popularData = items.filter(item => item.category === 'popular')
-                setMenu(popularData)
-            })
-    }, [])
+    const [menu] = useMenu();
+    const popular = menu?.filter(item => item.category === 'popular')
 
 
 
@@ -23,7 +25,7 @@ const FromOurMenu = () => {
 
             <div className='grid md:grid-cols-2 gap-8 grid-cols-1'>
                 {
-                    menu?.map(item => <MenuitemCard
+                    popular?.map(item => <MenuitemCard
                         key={item._id}
                         item={item}
                     ></MenuitemCard>)
