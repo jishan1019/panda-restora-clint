59cd7cb277aebe41cb1fceddf8668c29ac59039d
Your Name <imranislamjishan200@gmaile.com>
2023-06-01 22:46:27 +0600

    user post on database done

--- src/DashBoardInterface/DrowerLayout/DrowerNavigate.jsx
+++ src/DashBoardInterface/DrowerLayout/DrowerNavigate.jsx
@@ -1,128 +1,157 @@
-import React from 'react';
-import { Link, NavLink, Outlet } from 'react-router-dom';
-import { FaShoppingCart, FaWallet, FaCalendar, FaHome, FaComment } from 'react-icons/fa';
-import { GiHamburgerMenu } from 'react-icons/gi';
+import React from "react";
+import { Link, NavLink, Outlet } from "react-router-dom";
+import {
+  FaShoppingCart,
+  FaWallet,
+  FaCalendar,
+  FaHome,
+  FaComment,
+} from "react-icons/fa";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { useCart } from "../../CustomHooks/useCart";
 
 const DrowerNavigate = () => {
-    return (
-        <div className="drawer drawer-mobile bg-[#F6F6F6] ">
-            <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-            <div className="drawer-content flex flex-col items-center justify-center">
-                <Outlet />
-                <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
+  const [cart] = useCart();
 
-            </div>
-            <div className="drawer-side">
-                <label htmlFor="my-drawer-2 " className="drawer-overlay"></label>
-                <ul className="menu bg-[#FF7F0E] p-4 w-80 text-base-content">
-                    <li className='text-2xl font-bold mb-6'>
-                        <h1>
-                            Bistro Boss <br /> Restudent
-                        </h1>
-                    </li>
+  return (
+    <div className="drawer drawer-mobile bg-[#F6F6F6] ">
+      <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+      <div className="drawer-content flex flex-col items-center justify-center">
+        <Outlet />
+        <label
+          htmlFor="my-drawer-2"
+          className="btn btn-primary drawer-button lg:hidden"
+        >
+          Open drawer
+        </label>
+      </div>
+      <div className="drawer-side">
+        <label htmlFor="my-drawer-2 " className="drawer-overlay"></label>
+        <ul className="menu bg-[#FF7F0E] p-4 w-80 text-base-content">
+          <li className="text-2xl font-bold mb-6">
+            <h1>
+              Bistro Boss <br /> Restudent
+            </h1>
+          </li>
 
-                    <li>
-                        <NavLink
-                            to='/'
-                            aria-label='Home'
-                            title='Home'
-                            className={({ isActive }) => (isActive ? 'active font-bold text-white bg-[#FF7F0E]' : '')}
-                        >
-                            <FaHome />
-                            User Home
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink
-                            to='/dashboard/myCart'
-                            aria-label='Home'
-                            title='Home'
-                            className={({ isActive }) => (isActive ? 'active font-bold text-white bg-[#FF7F0E]' : '')}
-                        >
-                            <FaShoppingCart />
-                            My Cart
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink
-                            to='/'
-                            aria-label='Home'
-                            title='Home'
-                            className={({ isActive }) => (isActive ? 'active font-bold text-white bg-[#FF7F0E]' : '')}
-                        >
-                            <FaCalendar />
-                            Reservation
-                        </NavLink>
-                    </li>
+          <li>
+            <NavLink
+              to="/"
+              aria-label="Home"
+              title="Home"
+              className={({ isActive }) =>
+                isActive ? "active font-bold text-white bg-[#FF7F0E]" : ""
+              }
+            >
+              <FaHome />
+              User Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/dashboard/myCart"
+              aria-label="Home"
+              title="Home"
+              className={({ isActive }) =>
+                isActive ? "active font-bold text-white bg-[#FF7F0E]" : ""
+              }
+            >
+              <FaShoppingCart />
+              My Cart
+              <div className="badge badge-secondary">+{cart?.length || 0}</div>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/"
+              aria-label="Home"
+              title="Home"
+              className={({ isActive }) =>
+                isActive ? "active font-bold text-white bg-[#FF7F0E]" : ""
+              }
+            >
+              <FaCalendar />
+              Reservation
+            </NavLink>
+          </li>
 
-                    <li>
-                        <NavLink
-                            to='/'
-                            aria-label='Home'
-                            title='Home'
-                            className={({ isActive }) => (isActive ? 'active font-bold text-white bg-[#FF7F0E]' : '')}
-                        >
-                            <FaWallet />
-                            Payment History
-                        </NavLink>
-                    </li>
+          <li>
+            <NavLink
+              to="/"
+              aria-label="Home"
+              title="Home"
+              className={({ isActive }) =>
+                isActive ? "active font-bold text-white bg-[#FF7F0E]" : ""
+              }
+            >
+              <FaWallet />
+              Payment History
+            </NavLink>
+          </li>
 
-                    <div className="bg-white rounded-md h-[2px] divider"></div>
+          <div className="bg-white rounded-md h-[2px] divider"></div>
 
-                    <li>
-                        <NavLink
-                            to='/'
-                            aria-label='Home'
-                            title='Home'
-                            className={({ isActive }) => (isActive ? 'active font-bold text-white bg-[#FF7F0E]' : '')}
-                        >
-                            <FaHome />
-                            Home
-                        </NavLink>
-                    </li>
+          <li>
+            <NavLink
+              to="/"
+              aria-label="Home"
+              title="Home"
+              className={({ isActive }) =>
+                isActive ? "active font-bold text-white bg-[#FF7F0E]" : ""
+              }
+            >
+              <FaHome />
+              Home
+            </NavLink>
+          </li>
 
-                    <li>
-                        <NavLink
-                            to='/'
-                            aria-label='Home'
-                            title='Home'
-                            className={({ isActive }) => (isActive ? 'active font-bold text-white bg-[#FF7F0E]' : '')}
-                        >
-                            <GiHamburgerMenu />
-                            Menu
-                        </NavLink>
-                    </li>
+          <li>
+            <NavLink
+              to="/"
+              aria-label="Home"
+              title="Home"
+              className={({ isActive }) =>
+                isActive ? "active font-bold text-white bg-[#FF7F0E]" : ""
+              }
+            >
+              <GiHamburgerMenu />
+              Menu
+            </NavLink>
+          </li>
 
-                    <li>
-                        <NavLink
-                            to='/'
-                            aria-label='Home'
-                            title='Home'
-                            className={({ isActive }) => (isActive ? 'active font-bold text-white bg-[#FF7F0E]' : '')}
-                        >
-                            <FaShoppingCart />
-                            Shop
-                        </NavLink>
-                    </li>
+          <li>
+            <NavLink
+              to="/"
+              aria-label="Home"
+              title="Home"
+              className={({ isActive }) =>
+                isActive ? "active font-bold text-white bg-[#FF7F0E]" : ""
+              }
+            >
+              <FaShoppingCart />
+              Shop
+            </NavLink>
+          </li>
 
-                    <li>
-                        <NavLink
-                            to='/'
-                            aria-label='Home'
-                            title='Home'
-                            className={({ isActive }) => (isActive ? 'active font-bold text-white bg-[#FF7F0E]' : '')}
-                        >
-                            <FaComment />
-                            Contact
-                        </NavLink>
-                    </li>
+          <li>
+            <NavLink
+              to="/"
+              aria-label="Home"
+              title="Home"
+              className={({ isActive }) =>
+                isActive ? "active font-bold text-white bg-[#FF7F0E]" : ""
+              }
+            >
+              <FaComment />
+              Contact
+            </NavLink>
+          </li>
 
-                    <div className="bg-white rounded-md h-[2px] divider"></div>
-                </ul>
-
-            </div>
-        </div>
-    );
+          <div className="bg-white rounded-md h-[2px] divider"></div>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default DrowerNavigate;
--- src/component/LoginRegestationLayoout/Regestation/Regestation.jsx
+++ src/component/LoginRegestationLayoout/Regestation/Regestation.jsx
@@ -1,113 +1,148 @@
-import React from 'react';
+import React from "react";
 import { useForm } from "react-hook-form";
-import img from "../../../assets/assets/others/authentication.png"
-import img1 from "../../../assets/assets/others/authentication2.png"
-import { Link, useNavigate } from 'react-router-dom';
-import { useContext } from 'react';
-import { AuthContext } from '../../Provider/AuthProvider';
-import Swal from 'sweetalert2'
-import { Helmet } from 'react-helmet-async';
-
+import img from "../../../assets/assets/others/authentication.png";
+import img1 from "../../../assets/assets/others/authentication2.png";
+import { Link, useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../../Provider/AuthProvider";
+import Swal from "sweetalert2";
+import { Helmet } from "react-helmet-async";
 
 const Regestation = () => {
+  const { createUser, updateUserProfile } = useContext(AuthContext);
+  const navigate = useNavigate();
 
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => {
+    const email = data?.email;
+    const password = data?.password;
+    const name = data?.name;
 
-    const { createUser, updateUserProfile } = useContext(AuthContext);
-    const navigate = useNavigate();
+    createUser(email, password)
+      .then((result) => {
+        const user = result.user;
+        updateUserProfile(data.name, data.photoURL)
+          .then(() => {
+            // reset();
+            // Swal.fire("Regestation Success!", "Go Back", "success");
+            //   navigate("/");
 
+            //User Eamil Post On Server Code Here---------------
+            const saveUser = { name: name, email: email };
 
-    const { register, handleSubmit, reset, formState: { errors } } = useForm();
-    const onSubmit = data => {
-        const email = data?.email;
-        const password = data?.password;
-
-        createUser(email, password)
-            .then(result => {
-                const user = result.user;
-                updateUserProfile(data.name, data.photoURL)
-                    .then(() => {
-                        reset();
-                        Swal.fire(
-                            'Regestation Success!',
-                            'Go Back',
-                            'success'
-                        )
-                        navigate('/');
-
-                    })
-                    .catch(error => console.log(error))
-
-
-
-            }).catch(error => {
-                Swal.fire(
-                    'Regestation Fail!',
-                    `${error}`,
-                    'info'
-                )
+            fetch("http://localhost:4000/users", {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify(saveUser),
             })
-    };
+              .then((res) => res.json())
+              .then((result) => {
+                reset();
+                Swal.fire("Registration Success!", "Go Back", "success");
+                navigate("/");
+              })
+              .catch((error) => {
+                console.error("Error:", error);
+              });
+          })
+          .catch((error) => console.log(error));
+      })
+      .catch((error) => {
+        Swal.fire("Regestation Fail!", `${error}`, "info");
+      });
+  };
 
+  return (
+    <>
+      <Helmet>
+        <title>Bistro | Regestation</title>
+      </Helmet>
+      <div
+        className="hero min-h-[80vh] mt-16"
+        style={{ backgroundImage: `url(${img})` }}
+      >
+        <div className="hero-overlay bg-opacity-0"></div>
+        <div className="hero-content text-center text-neutral-content">
+          <div className="w-full flex justify-center items-center md:flex-row flex-col-reverse p-8">
+            <div className="w-full space-y-4  md:w-[50%] text-black">
+              <h1 className="mb-8 mt-8 md:mt-0 font-bold text-3xl">
+                Regestation
+              </h1>
 
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <input
+                  className="bg-white w-full p-4 rounded-md block"
+                  placeholder="Enter Your Name"
+                  {...register("name", { required: true })}
+                />
 
-    return (
-        <>
-            <Helmet>
-                <title>Bistro | Regestation</title>
-            </Helmet>
-            <div className="hero min-h-[80vh] mt-16" style={{ backgroundImage: `url(${img})` }}>
-                <div className="hero-overlay bg-opacity-0"></div>
-                <div className="hero-content text-center text-neutral-content">
-                    <div className="w-full flex justify-center items-center md:flex-row flex-col-reverse p-8">
+                <input
+                  className="bg-white w-full p-4 rounded-md block mt-6"
+                  placeholder="Enter Your Email"
+                  {...register("email", { required: true })}
+                />
 
+                <input
+                  className="bg-white w-full p-4 rounded-md block mt-6"
+                  placeholder="Enter Your Password"
+                  {...register("password", { required: true })}
+                />
 
-                        <div className='w-full space-y-4  md:w-[50%] text-black'>
+                <input
+                  className="bg-white w-full p-4 rounded-md block mt-6"
+                  placeholder="Enter Your Photo Url"
+                  {...register("photoURL", { required: true })}
+                />
 
-                            <h1 className='mb-8 mt-8 md:mt-0 font-bold text-3xl'>Regestation</h1>
+                {errors.exampleRequired && <span>This field is required</span>}
 
-                            <form onSubmit={handleSubmit(onSubmit)}>
-                                <input className='bg-white w-full p-4 rounded-md block' placeholder='Enter Your Name'  {...register("name", { required: true })} />
+                <input
+                  className="p-4 bg-[#DBB984] w-full mt-6 font-semibold"
+                  type="submit"
+                  value="Sign Up"
+                />
+              </form>
 
-                                <input className='bg-white w-full p-4 rounded-md block mt-6' placeholder='Enter Your Email'  {...register("email", { required: true })} />
+              <p className="font-semibold text-[#DBB984]">
+                Alrady Regestared? <Link to="/login">Go to login</Link>
+              </p>
 
+              <p>Or Sign in With</p>
 
-                                <input className='bg-white w-full p-4 rounded-md block mt-6' placeholder='Enter Your Password' {...register("password", { required: true })} />
+              <div className="flex justify-center items-center space-x-3">
+                <button className="btn btn-circle btn-outline">
+                  <img
+                    className="p-1"
+                    src="https://i.ibb.co/NY7LtWM/images-removebg-preview.png"
+                    alt=""
+                  />
+                </button>
 
-
-                                <input className='bg-white w-full p-4 rounded-md block mt-6' placeholder='Enter Your Photo Url' {...register("photoURL", { required: true })} />
-
-
-
-                                {errors.exampleRequired && <span>This field is required</span>}
-
-                                <input className='p-4 bg-[#DBB984] w-full mt-6 font-semibold' type="submit" value="Sign Up" />
-                            </form>
-
-                            <p className='font-semibold text-[#DBB984]'>Alrady Regestared? <Link to='/login'>Go to login</Link></p>
-
-                            <p>Or Sign in With</p>
-
-                            <div className='flex justify-center items-center space-x-3'>
-                                <button className="btn btn-circle btn-outline">
-                                    <img className='p-1' src="https://i.ibb.co/NY7LtWM/images-removebg-preview.png" alt="" />
-                                </button>
-
-                                <button className="btn btn-circle btn-outline">
-                                    <img className='p-2' src="https://i.ibb.co/NKkfhCp/Git-Hub-Mark-removebg-preview.png" alt="" />
-                                </button>
-                            </div>
-
-                        </div>
-
-                        <div className='w-full md:w-[50%]'>
-                            <img className='w-[100vw]' src={`${img1}`} alt="" />
-                        </div>
-
-                    </div>
-                </div>
+                <button className="btn btn-circle btn-outline">
+                  <img
+                    className="p-2"
+                    src="https://i.ibb.co/NKkfhCp/Git-Hub-Mark-removebg-preview.png"
+                    alt=""
+                  />
+                </button>
+              </div>
             </div>
-        </>
-    );
+
+            <div className="w-full md:w-[50%]">
+              <img className="w-[100vw]" src={`${img1}`} alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Regestation;
